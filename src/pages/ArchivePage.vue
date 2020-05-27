870c30d6d7e01eb70d73827be5ee745634679116
unknown <koldream2233@gmail.com>
2020-05-27 16:32:45 +0700

    workshop6

--- src/pages/ArchivePage.vue
+++ src/pages/ArchivePage.vue
@@ -3,11 +3,16 @@
 </template>
 
 <script>
+import axios from 'axios'
 import CardList from '../components/CardList.vue'
 export default {
     name: 'ArchivePage',
     components:{
         CardList
+    },
+    async created(){
+        const { data } = await axios.get(`http://localhost:3000/archives`)
+        this.archives = data
     },
     data(){
         return{
